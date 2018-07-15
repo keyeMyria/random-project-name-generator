8d83a7e115accfbb2e9343ff9eb1b92735eec14a
Christoph Stach <christoph.stach@gmail.com>
2018-07-15 16:35:09 +0200

    removed unused code

--- server/src/users/controllers/users/users.controller.ts
+++ server/src/users/controllers/users/users.controller.ts
@@ -1,14 +1,12 @@
-import { BadRequestException, Body, Controller, Get, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import { UsersService } from '../../services/users/users.service';
 import { RegisterRequest } from '../../requests/register.request';
 import { RegisterResponse } from '../../responses/register.response';
 import { LoginResponse } from '../../responses/login.response';
 import { LoginRequest } from '../../requests/login.request';
-import { User, UserRole } from '../../models/user.model';
-import { EnumHelper } from '../../../shared/helpers/enum.helper';
+import { User } from '../../models/user.model';
 import { MapperService } from '../../../shared/services/mapper/mapper.service';
 import { UserDto } from '../../dtos/user.dto';
-import { FindAllResponse } from '../../responses/find-all.response';
 
 @Controller('users')
 export class UsersController {
@@ -19,10 +17,10 @@ export class UsersController {
   async register(@Body() registerRequest: RegisterRequest): Promise<RegisterResponse> {
     if (registerRequest.username && registerRequest.email && registerRequest.password) {
       const user: User = await this.usersService.register(registerRequest);
-      const userViewModel: UserDto = this.mapperService.map<User, UserDto>(user, UserDto);
+      const userDto: UserDto = this.mapperService.map<User, UserDto>(user, UserDto);
 
       return {
-        data: userViewModel,
+        data: userDto,
       };
     } else {
       throw new BadRequestException();
@@ -40,41 +38,5 @@ export class UsersController {
     } else {
       throw new BadRequestException();
     }
-  }
-
-  @Get('test-register')
-  async testRegister(): Promise<RegisterResponse> {
-    await this.usersService.clearAll();
-    return this.register({
-      email: 'christoph.stach@ŋmail.com',
-      password: '123456',
-      username: 'christophstach',
-      role: UserRole.SUPERADMIN,
-      firstName: 'Christoph',
-      lastName: 'Stach',
-    });
-  }
-
-  @Get('test-login')
-  async testLogin(): Promise<LoginResponse> {
-    return this.login({
-      email: 'christoph.stach@ŋmail.com',
-      password: '123456',
-    });
-  }
-
-  @Get()
-  async findAll(): Promise<FindAllResponse> {
-    const users: User[] = await this.usersService.findAll();
-    const userViewModels: UserDto[] = users.map((user: User) => this.mapperService.map<User, UserDto>(user, UserDto));
-
-    return {
-      data: userViewModels,
-    };
-  }
-
-  @Get('test')
-  async test() {
-    return EnumHelper.values(UserRole);
   }
 }
